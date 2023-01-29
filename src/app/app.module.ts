29e13fd0c8dff1d8b5943c32c25d76cc10c49089
Luke Enderby <luke@djpinhead.com>
2023-01-29 15:27:44 +0000

    form working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { InputComponent } from './components/input/input.component';
@@ -15,7 +15,8 @@ import { messagesReducer } from "./state/messages/messages.reducer";
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({ messages: messagesReducer })
+    StoreModule.forRoot({ messages: messagesReducer }),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from "@ngrx/store";
+import {addMessage} from "../../state/messages/messages.actions";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
+export class InputComponent implements OnInit {
+
+  public form: FormGroup;
+
+  constructor( private store: Store, private formBuilder: FormBuilder ) {
+    this.form = this.formBuilder.group({
+      message: ['']
+    });
+  }
+
+  ngOnInit() {
+    this.store.dispatch(addMessage({message: 'something'}));
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.store.dispatch(addMessage(this.form.value));
+      this.form.reset();
+    }
+  }
 
 }
