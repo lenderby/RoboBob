da4e49ad61a5bf4f70ebe2a137e8c6b36716e37a
Luke Enderby <luke@djpinhead.com>
2023-01-30 16:19:12 +0000

    tweaks

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectAllMessages } from "../../state/messages/messages.selectors";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-chat',
@@ -8,7 +9,11 @@ import { selectAllMessages } from "../../state/messages/messages.selectors";
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
-  public allMessages$ = this.store.select(selectAllMessages);
+  public allMessages$ = this.store.select(selectAllMessages).pipe(tap(() => {
+    setTimeout(function() {
+      window.scrollTo(0, document.body.scrollHeight); // automatically scroll the chat
+    });
+  }));
   public welcomeMessage: string = `Hey, I'm RoboBob. You can ask me a question about myself, or I can do simple maths..`;
 
   constructor(private store: Store) {}
